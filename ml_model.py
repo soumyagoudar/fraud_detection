28bd2f3568f99bf706fe96db39e2f2b0f3901ec2
soumyagoudar <soumya333goudar@gmail.com>
2023-07-03 00:07:33 +0530

    added 3 new ml model

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,12 @@
 
 
 print("adding all ml related code here")
+
+def model1():
+    print("adding code for ML model1")
+
+def model2():
+    print("adding code for ML model2")
+
+def model3():
+    print("adding code for ML model3")
